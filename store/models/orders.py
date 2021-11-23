b4d8aad5ee091ecf2f8b24de0fb770b57303ce77
cesarcarlosc <fccampos@idat.edu.pe>
2021-11-23 12:31:37 -0500

    Initial commit

---
+++ store/models/orders.py
@@ -0,0 +1,24 @@
+from django.db import models
+from .customer import Customer
+from .product import Products
+import datetime
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    customer = models.ForeinKey(Customer, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=9,default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def placeOrder(self):
+        self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).oder_by('date')
